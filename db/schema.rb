ba21f3c2dd968a013ed35f3979df058f02d0126c
Brooks Swinnerton <bswinnerton@gmail.com>
2013-01-12 12:59:52 -0500

    adding time of party

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112174934) do
+ActiveRecord::Schema.define(:version => 20130112175727) do
 
   create_table "parties", :force => true do |t|
     t.string   "host"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130112174934) do
     t.string   "number"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "time"
   end
 
   create_table "users", :force => true do |t|
