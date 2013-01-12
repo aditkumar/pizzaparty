186ac635a8bb2192e3c0e46365eacbed02c7e2f6
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-01-12 12:58:46 -0500

    Routes for facebook omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Pizzaparty::Application.routes.draw do
   root :to => "parties#new"
   resources :parties
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
