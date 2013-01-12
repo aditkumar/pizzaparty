0c8c1a86da5cbfdf2fb65db6a6e816136e4c339c
Brooks Swinnerton <bswinnerton@gmail.com>
2013-01-12 11:30:04 -0500

    default rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pizzaparty::Application.config.secret_token = 'c5d1b6d9bd60993bcad401d0c5e62bf319ce3c7f83ff7d95ae34240f52ee120e109e7cce1202b83d8e1c962d07589aff4d68e9d8bd25350be4ba19c5de206cd4'
